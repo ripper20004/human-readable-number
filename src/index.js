811b02c99068987d3bd5b2d57bf91c0670210daa
Zata2ctra <116357056+Zara2ctra@users.noreply.github.com>
2023-07-07 16:06:38 +0400

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const lessThenTwenty = ['zero','one', 'two', 'three', 'four', 'five',
+    'six', 'seven','eight', 'nine', 'ten', 'eleven', 'twelve',
+    'thirteen', 'fourteen', 'fifteen', 'sixteen',
+    'seventeen', 'eighteen', 'nineteen'];
+
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty',
+    'seventy', 'eighty', 'ninety'];
+
 module.exports = function toReadable (number) {
-  
+    if (number <= 19) return lessThenTwenty[number];
+    if (number <= 99) {
+        return number % 10 === 0 ? tens[number / 10 | 0]
+            : tens[number / 10 | 0] + " " + lessThenTwenty[number % 10];
+    }
+    return number % 100 === 0 ? lessThenTwenty[number / 100 | 0] + 'hundred'
+        : lessThenTwenty[number / 100 | 0] + 'hundred' + toReadable(number % 100);
 }
